48358e3f4d09466c9671fcca56e6d16d530af7f1
Jacob Menashi <jbmenashi@gmail.com>
2018-08-14 01:01:36 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Beetles: 'Revoluton' };
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(obj,key) {
+  delete obj.key;
+}
